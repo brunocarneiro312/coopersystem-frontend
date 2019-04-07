ddb5fd900a7892a1f91c8016521646ad08e4c36c
bruno.carneiro <bruno.carneiro@gmail.com>
2019-04-07 12:21:14 -0300

    primeiro commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CustomHeaderComponent } from './custom-header/custom-header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CustomHeaderComponent
   ],
   imports: [
     BrowserModule,
