81d8a7fcd175ccb5408ae76a0c5369ea53e9c810
bruno.carneiro <bruno.carneiro@gmail.com>
2019-04-07 12:39:40 -0300

    setup

---
+++ src/app/services/coopersystem.api.ts
@@ -0,0 +1,2 @@
+// Armazena URI de acesso ao backend
+export const COOPERSYSTEM_API: string = 'http://localhost:8080';
--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -1,9 +1,57 @@
 import { Injectable } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {User} from '../model/user.model';
+import {COOPERSYSTEM_API} from './coopersystem.api';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
 
-  constructor() { }
+  constructor(private http: HttpClient) {
+
+  }
+
+  /**
+   * Realiza o login
+   * @param user: usuário a ser logado
+   */
+  login(user: User) {
+    return this.http.post(`${COOPERSYSTEM_API}/auth`, user);
+  }
+
+  /**
+   * Cria ou altera usuário
+   * @param user
+   */
+  createOrUpdate(user: User) {
+    if (user.id != null && user.id != '') {
+      return this.http.put(`${COOPERSYSTEM_API}/api/user`, user);
+    }
+    else {
+      user.id = null;
+      return this.http.post(`${COOPERSYSTEM_API}/api/user`, user);
+    }
+  }
+
+  /**
+   * Lista todos os usuários cadastrados
+   * @param page
+   * @param count
+   */
+  findAll(page: number, count: number) {
+    return this.http.get(`${COOPERSYSTEM_API}/api/user`);
+  }
+
+  /**
+   * Busca usuário por id
+   * @param id
+   */
+  findById(id: number) {
+    return this.http.get(`${COOPERSYSTEM_API}/api/user/${id}`);
+  }
+
+  delete(id: number) {
+    return this.http.delete(`${COOPERSYSTEM_API}/api/user/${id}`);
+  }
 }
