ab93fa57e834310f25c7c368a00132bdb8d36f74
bruno.carneiro <bruno.carneiro@gmail.com>
2019-04-07 12:41:32 -0300

    setup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,15 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CustomHeaderComponent } from './custom-header/custom-header.component';
+import { HomeComponent } from './components/home/home.component';
+import { LoginComponent } from './components/security/login/login.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CustomHeaderComponent
+    CustomHeaderComponent,
+    HomeComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
