5867ff4b7db188abb470196a243bf3432521b7dd
bruno.carneiro <bruno.carneiro@gmail.com>
2019-04-08 14:52:03 -0300

    finalizando implementação

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,6 +1,7 @@
 import {Component, OnInit} from '@angular/core';
 import {UserService} from '../../services/user.service';
 import {Router} from '@angular/router';
+import {SharedService} from '../../services/shared.service';
 
 @Component({
   selector: 'app-home',
@@ -10,11 +11,13 @@ import {Router} from '@angular/router';
 export class HomeComponent implements OnInit {
 
   users = undefined;
-
   userDetail = undefined;
+  public shared: SharedService;
+  message: string;
+  responseStatus: string;
 
   constructor(private userService: UserService, private route: Router) {
-
+    this.shared = SharedService.getIntance();
   }
 
   ngOnInit() {
@@ -29,9 +32,30 @@ export class HomeComponent implements OnInit {
 
   }
 
+  deletarUsuario(userId) {
+    this.userService.delete(userId).subscribe(response => {
+      this.listarUsuarios();
+      this.message = 'Usuário removido.';
+    }, err => {
+      this.message = 'Erro ao remover usuário';
+      this.responseStatus = err.status;
+    });
+  }
+
   showDetails(userId) {
     this.userService.findById(userId).subscribe(response => {
       this.userDetail = response;
     });
+  }
+
+  isAdmin(): boolean {
+    if (this.shared.user) {
+      if (this.shared.user.authorities.length > 0) {
+        if (this.shared.user.authorities[0].authority.substring(5) === 'ADMIN') {
+          return true;
+        }
+      }
+    }
+    return false;
   }
 }
